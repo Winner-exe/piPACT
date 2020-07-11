76dc4d03e77567a33666cfbce069e3635a9220ee
Winston Lee <winstonleemail@gmail.com>
2020-07-11 17:15:25 -0400

    Added Naive Bayes KDE Classifier class.

---
+++ kde-classifier.py
@@ -0,0 +1,42 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.neighbors import KernelDensity
+import numpy as np
+
+
+# noinspection PyAttributeOutsideInit,PyPep8Naming
+class KDEClassifier(BaseEstimator, ClassifierMixin):
+    """Bayesian generative classification based on KDE
+    Code used from Chapter 5 of the Python Data Science Handbook by Jake VanderPlas:
+    https://jakevdp.github.io/PythonDataScienceHandbook/05.13-kernel-density-estimation.html
+
+    Parameters
+    ----------
+    bandwidth : float
+        the kernel bandwidth within each class
+    kernel : str
+        the kernel name, passed to KernelDensity
+    """
+
+    def __init__(self, bandwidth=1.0, kernel='gaussian'):
+        self.bandwidth = bandwidth
+        self.kernel = kernel
+
+    def fit(self, X, y):
+        self.classes_ = np.sort(np.unique(y))
+        training_sets = [X[y == yi] for yi in self.classes_]
+        self.models_ = [KernelDensity(bandwidth=self.bandwidth,
+                                      kernel=self.kernel).fit(Xi)
+                        for Xi in training_sets]
+        self.logpriors_ = [np.log(Xi.shape[0] / X.shape[0])
+                           for Xi in training_sets]
+        return self
+
+    # noinspection PyUnresolvedReferences
+    def predict_proba(self, X):
+        logprobs = np.array([model.score_samples(X)
+                             for model in self.models_]).T
+        result = np.exp(logprobs + self.logpriors_)
+        return result / result.sum(1, keepdims=True)
+
+    def predict(self, X):
+        return self.classes_[np.argmax(self.predict_proba(X), 1)]

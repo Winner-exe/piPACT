373a5206bd1444b9afaa62c6a33307ab3557bc83
Winston Lee <winstonleemail@gmail.com>
2020-07-14 01:24:56 -0400

    Updated documentation.

--- pi_pact_sort.py
+++ pi_pact_sort.py
@@ -1,7 +1,7 @@
 def categorize(distance: float) -> int:
-    """Distance sorting method to be referenced across all data analysis files."""
+    """Distance binning method to be referenced across all data analysis files and classifiers."""
 
     if distance < 2:
-        return 0
-    else:
         return 1
+    else:
+        return 0

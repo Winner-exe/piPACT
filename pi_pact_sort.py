e8433d3114a3fc475200b59e8d624e9d3b3212e8
Winston Lee <winstonleemail@gmail.com>
2020-07-15 23:46:33 -0400

    Changed categorization function to try guessing within a meter of accuracy.

--- pi_pact_sort.py
+++ pi_pact_sort.py
@@ -1,7 +1,6 @@
+import math
+
+
 def categorize(distance: float) -> int:
     """Distance binning method to be referenced across all data analysis files and classifiers."""
-
-    if distance < 2:
-        return 1
-    else:
-        return 0
+    return math.floor(distance)
